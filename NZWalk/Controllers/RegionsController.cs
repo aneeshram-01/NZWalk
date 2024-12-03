359c3e70c7d1a8d2ed57b04cad16a4bb74aa2368
Aneeshram Bhat <aneeshram19@gmail.com>
2024-12-03 15:32:12 +0530

    Add project files.

---
+++ NZWalk/Controllers/RegionsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalk.Data;
+
+namespace NZWalk.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalkDbContext dbContext;
+
+        public RegionsController(NZWalkDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        //GET ALL REGIONS
+        //GET: https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var region = dbContext.Regions.ToList();
+
+            return Ok(region);
+        }
+
+
+        //GET SINGLE REGIONS (GET Region by Id)
+        //GET: https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            //var region = dbContext.Regions.Find(id);
+
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (region == null)
+            { 
+                return NotFound(); 
+            }
+
+            return Ok(region); 
+        }
+
+    }
+}
---
+++ NZWalk/Data/NZWalkDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalk.Models.Domain;
+
+namespace NZWalk.Data
+{
+    public class NZWalkDbContext : DbContext
+    {
+        public NZWalkDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
+    }
+}
